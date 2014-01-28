f856c3c5cf3b23d37c81cba9507fcde3df9b8d3b
Antony Jones <antony.jones@news.co.uk>
2014-01-28 10:36:31 +0000

    Update BuildConfig to remove old repository.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,8 +1,6 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
-grails.project.repos.splendid.url = "http://192.168.0.137/artifactory/grails-plugins"
-grails.project.repos.default = "splendid"
 
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
